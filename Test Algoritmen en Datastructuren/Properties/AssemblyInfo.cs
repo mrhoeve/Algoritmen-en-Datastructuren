f57193fb1c06a23fc0b6e80d135cccdc019462a0
Marc Hoeve <mrhoeve@gmail.com>
2019-02-02 13:47:44 +0100

    Places testproject in datafolder for synchronising ;-)

---
+++ Test Algoritmen en Datastructuren/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test Algoritmen en Datastructuren")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test Algoritmen en Datastructuren")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7f43279-8e96-4660-bf89-3268673702fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test Algoritmen en Datastructuren/TestMyArrayList.cs
@@ -0,0 +1,104 @@
+﻿using Algoritmen_en_Datastructuren;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_Algoritmen_en_Datastructuren
+{
+    [TestClass]
+    public class TestMyArrayList
+    {
+        [TestMethod]
+        public void CreateMyArrayList()
+        {
+            MyArrayList _myArray = new MyArrayList(10);
+            Assert.IsNotNull(_myArray);
+        }
+
+        [TestMethod]
+        public void CreateArrayListOfTwoAndPutTwoItemsInIt()
+        {
+            MyArrayList _myArray = new MyArrayList(2);
+            _myArray.add(4);
+            _myArray.add(8);
+            Assert.AreEqual(_myArray.get(0), 4);
+            Assert.AreEqual(_myArray.get(1), 8);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CreateArrayListOfTwoPutTwoItemsInItAndAskForNumberThree()
+        {
+            MyArrayList _myArray = new MyArrayList(2);
+            _myArray.add(4);
+            _myArray.add(8);
+            int test = _myArray.get(2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CreateArrayListOfTwoPutTwoItemsInItAndTryToAddItemThree()
+        {
+            MyArrayList _myArray = new MyArrayList(2);
+            _myArray.add(4);
+            _myArray.add(8);
+            _myArray.add(10);
+        }
+
+        [TestMethod]
+        public void CreateArrayListOfTwoPutTwoItemsInItClearListAddAnotherTwoAndCheckTheValues()
+        {
+            MyArrayList _myArray = new MyArrayList(2);
+            _myArray.add(4);
+            _myArray.add(8);
+            Assert.AreEqual(_myArray.get(0), 4);
+            Assert.AreEqual(_myArray.get(1), 8);
+            _myArray.clear();
+            _myArray.add(5);
+            _myArray.add(10);
+            Assert.AreEqual(_myArray.get(1), 10);
+            Assert.AreEqual(_myArray.get(0), 5);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CreateArrayListAddItemAskForNonaddedIndex()
+        {
+            MyArrayList _myArray = new MyArrayList(2);
+            _myArray.set(1, 6);
+            int test = _myArray.get(0);
+        }
+
+        [TestMethod]
+        public void CreateArrayFillItAndCountTheNumbers()
+        {
+            MyArrayList _myArray = new MyArrayList(10);
+            _myArray.add(4);
+            _myArray.add(8);
+            _myArray.add(7);
+            _myArray.add(3);
+            _myArray.add(0);
+            _myArray.add(2);
+            _myArray.add(5);
+            _myArray.add(8);
+            _myArray.add(9);
+            _myArray.add(8);
+            Assert.AreEqual(_myArray.countOccurences(8), 3);
+            Assert.AreEqual(_myArray.countOccurences(1), 0);
+            _myArray.clear();
+            _myArray.add(4);
+            _myArray.add(1);
+            _myArray.add(7);
+            _myArray.add(3);
+            _myArray.add(0);
+            _myArray.add(2);
+            _myArray.add(5);
+            _myArray.add(1);
+            _myArray.add(9);
+            _myArray.add(1);
+            Assert.AreEqual(_myArray.countOccurences(8), 0);
+            Assert.AreEqual(_myArray.countOccurences(1), 3);
+        }
+
+    }
+}
