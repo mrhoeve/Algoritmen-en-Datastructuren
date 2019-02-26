b4ed24e0363076fc3f27158702f5144c4a26c9c5
Marc Hoeve <mrhoeve@gmail.com>
2019-02-26 15:31:33 +0100

    Added implementation of factional

---
+++ Algoritmen en Datastructuren/Homework/Opgave_3_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmen_en_Datastructuren.Homework
+{
+    public class Opgave_3_1
+    {
+        public Opgave_3_1()
+        {
+            Console.WriteLine(FaculteitNietRecursief(6));
+            Console.WriteLine(FaculteitRecursief(6));
+        }
+
+        public static int FaculteitRecursief(int getal)
+        {
+            if (getal == 1) return 1;
+            return getal * FaculteitRecursief(getal - 1);
+        }
+
+        public static int FaculteitNietRecursief(int getal)
+        {
+            if (getal == 1) return 1;
+            int antwoord = 1;
+            for(int teller=2;teller<=getal; teller++)
+            {
+                antwoord *= teller;
+            }
+            return antwoord;
+        }
+    }
+}
---
+++ Test Algoritmen en Datastructuren/Homework/TestOpgave_3_1.cs
@@ -0,0 +1,18 @@
+﻿using Algoritmen_en_Datastructuren.Homework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Algoritmen_en_Datastructuren.Homework
+{
+    /// <summary>
+    /// Summary description for TestOpgave_3_1
+    /// </summary>
+    [TestClass]
+    public class TestOpgave_3_1
+    {
+        [TestMethod]
+        public void TestFaculteit()
+        {
+            Assert.AreEqual(120, Opgave_3_1.FaculteitRecursief(5));
+        }
+    }
+}
