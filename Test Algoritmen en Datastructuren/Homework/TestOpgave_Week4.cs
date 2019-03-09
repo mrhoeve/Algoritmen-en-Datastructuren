e12934da72767b90b27c07a8f74f1d3e0a79d735
Marc Hoeve <mrhoeve@gmail.com>
2019-03-09 10:42:26 +0100

    Added unittest for tree

---
+++ Test Algoritmen en Datastructuren/Homework/TestOpgave_Week4.cs
@@ -0,0 +1,46 @@
+﻿using Algoritmen_en_Datastructuren.BinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Algoritmen_en_Datastructuren.Homework
+{
+    /// <summary>
+    /// Summary description for TestOpgave_4
+    /// </summary>
+    [TestClass]
+    public class TestOpgave_Week4
+    {
+        private BinaryTree<int> tree;
+
+        public TestOpgave_Week4()
+        {
+            // Set the working tree for all tests
+            tree = new BinaryTree<int>(4);
+            tree.GetRoot().setLeft(new BinaryNode<int>(2, new BinaryNode<int>(1, null, null), new BinaryNode<int>(3, null, null)));
+            tree.GetRoot().setRight(new BinaryNode<int>(6, null, null));
+        }
+
+        [TestMethod]
+        public void TestToStringMethod()
+        {
+            Assert.IsTrue("[ [ NULL 1 NULL ] 2 [ NULL 3 NULL ] ] 4 [ NULL 6 NULL ] ".Equals(tree.ToString()));
+        }
+
+        [TestMethod]
+        public void TestCountLeaves()
+        {
+            Assert.AreEqual(3, tree.CountLeaves());
+        }
+
+        [TestMethod]
+        public void TestCountNodesWithOneChild()
+        {
+            Assert.AreEqual(0, tree.CountNodesWithOneChild());
+        }
+
+        [TestMethod]
+        public void TestCountNodesWithTwoChildren()
+        {
+            Assert.AreEqual(2, tree.CountNodesWithTwoChildren());
+        }
+    }
+}
